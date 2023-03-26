7821ae2f75b0d9c3954c3b623bc3efb57fdfc194
James Wilhelm <jrw0594@gmail.com>
2023-03-26 01:48:28 -0400

    initial commit

---
+++ Scrambler/ScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScramblerApp.swift
+//  Scrambler
+//
+//  Created by James Wilhelm on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
